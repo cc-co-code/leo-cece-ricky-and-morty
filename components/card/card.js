acb9bc7f8b7d3be99182cb5e6d78393b97f69600
leonel-acosta <leoacosta88@gmail.com>
2024-09-24 14:58:35 +0200

    Update Card Component with Inner HTML structure

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,37 @@
-export function createCharacterCard() {}
+const cardContainer = document.querySelector("[data-js='card-container']")
+
+export function createCharacterCard() {
+
+FETCHDATAARRAY.forEach(element => { //TO BE REVIEWED AND UPDATED
+   const characterCard = document.createElement(li)
+   characterCard.classList.add("card")
+
+    characterCard.innerHTML = `
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${character.image}"
+              alt="${character.name}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${character.name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${character.status}</dd
+                >
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${character.description}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${character.ocurrences}</dd>
+            </dl>
+          </div>
+    `
+
+cardContainer.append(card)
+
+});
+    
+}
+
