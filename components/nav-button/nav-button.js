3a0f5d9260fd7f92b7efabd1e4533d1e779ae767
YOUR NAME <cecilia@bartlewski.de>
2024-09-25 20:09:09 +0200

    added footer in index.html, added some styling to respective css files but not finished yet, appended footer in respective js documents

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -1,7 +1,9 @@
 export function createNavButton(name, id) {
-    const button = document.createElement("button")
-    button.classList.add("button", `button--${id}`)
-    button.setAttribute("data-js",`button-${id}`)
-    button.textContent = `${name}`
-    return button
+  const footer = document.querySelector('[data-js="footer"]');
+  const button = document.createElement("button");
+  button.classList.add("button", `button--${id}`);
+  button.setAttribute("data-js", `button-${id}`);
+  button.textContent = `${name}`;
+  footer.append(button);
+  return button;
 }
--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,11 +1,18 @@
 import { createNavButton } from "../nav-button/nav-button.js";
 
-export function createNavPagination(onClickPrev, onClickNext, page, maxPage, createNavButtton) {
-
+export function createNavPagination(
+  onClickPrev,
+  onClickNext,
+  page,
+  maxPage,
+  createNavButtton
+) {
+  const footer = document.querySelector('[data-js="footer"]');
   const navPagination = document.createElement("nav");
-  document.body.append(navPagination);
+  footer.append(navPagination);
+  //document.body.append(navPagination);
   navPagination.classList.add("navigation");
-  navPagination.setAttribute('data-js',"navigation");
+  navPagination.setAttribute("data-js", "navigation");
 
   const buttonPrevious = createNavButton("previous", "prev");
   const buttonNext = createNavButton("next", "next");
@@ -15,15 +22,14 @@ export function createNavPagination(onClickPrev, onClickNext, page, maxPage, cre
   const pagination = document.createElement("span");
   navPagination.append(pagination);
   pagination.classList.add("navigation__pagination");
-  pagination.setAttribute("data-js","pagination");
+  pagination.setAttribute("data-js", "pagination");
   pagination.textContent = `${page} / ${maxPage}`;
 
-  navPagination.append(buttonNext)
+  navPagination.append(buttonNext);
 
   const prevButton = document.querySelector('[data-js="button-prev"]');
   const nextButton = document.querySelector('[data-js="button-next"]');
 
-  
-  prevButton.addEventListener("click", onClickPrev)
-  nextButton.addEventListener("click", onClickNext)
+  prevButton.addEventListener("click", onClickPrev);
+  nextButton.addEventListener("click", onClickNext);
 }
