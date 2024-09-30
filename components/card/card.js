8b643814e968c012857f1c87ef1a2944d0a6498d
leonel-acosta <leoacosta88@gmail.com>
2024-09-30 11:33:14 +0200

    search functions fixes

--- components/card/card.js
+++ components/card/card.js
@@ -14,15 +14,15 @@ export function createCharacterCard(character) {
           </div>
           <div class="card__content">
             <h2 class="card__title">${character.name}</h2>
-            <dl class="card__info">
-              <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">${character.status}</dd
+            <ul class="card__info">
+              <li class="card__info-title">Status</li>
+              <li class="card__info-description">${character.status}</
                 >
-              <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description">${character.type}</dd>
-              <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">${character.ocurrences}</dd>
-            </dl>
+              <li class="card__info-title">Type</li>
+              <li class="card__info-description">${character.type}</li>
+              <li class="card__info-title">Occurrences</li>
+              <li class="card__info-description">${character.episode.length}</li>
+            </ul>
           </div>
     `
     cardContainer.append(characterCard)
